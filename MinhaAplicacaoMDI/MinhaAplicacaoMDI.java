750f1b41ffbd4c05097249606d98615a18b885e2
Gabriel <110850579+gabriel-massambani@users.noreply.github.com>
2023-09-11 20:21:19 -0300

    Add files via upload

---
+++ MinhaAplicacaoMDI/MinhaAplicacaoMDI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MinhaAplicacaoMDI;
+
+/**
+ *
+ * @author Toni
+ */
+public class MinhaAplicacaoMDI {
+     public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
+    }
+    
+}
