3573fdfc036c3268be6dd6eae591ee3547d50ed6
Gabriel <110850579+gabriel-massambani@users.noreply.github.com>
2023-09-11 21:21:00 -0300

    Add files via upload

---
+++ exeSwingJava/src/main/java/com/mycompany/exeswingjava/ExeSwingJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exeswingjava;
+
+/**
+ *
+ * @author Toni
+ */
+public class ExeSwingJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
